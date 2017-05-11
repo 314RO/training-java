28a8e964418a6b11ba65c2072ee8c9c4640ffdcf
Pierre Salles <psalles@excilys.com>
2017-05-11 17:59:54 +0200

    Suppression de fichiers inutiles

--- src/main/java/Test.java
+++
@@ -1,8 +0,0 @@
-import com.excilys.training.ui.UserInterface;
-
-public class Test {
-    public static void main(String[] args) {
-       UserInterface ui = new UserInterface();
-        ui.menu();
-    }
-}
